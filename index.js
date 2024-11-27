dea6b9301c9c0c4f8afc17b378d4b20cff0de389
Amit Dhingra <amitdhingra180@gmail.com>
2024-11-27 16:30:54 +0530

    v3

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-require('dotenv').config(); // Load environment variables from .env
 const http = require('http');
-const port = process.env.PORT || 3000; // Use the PORT from .env or default to 3000
+const port = process.env.PORT || 3000; 
 
-// Bind to 0.0.0.0 for compatibility with Railway
+
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello');
 });
 
-server.listen(port, '0.0.0.0', () => {
+server.listen(port, () => {
     console.log(`Server running at http://0.0.0.0:${port}/`);
 });
